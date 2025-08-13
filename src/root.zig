9c50732523e393bfb2581db8c87c944a50701079
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-08-13 12:31:03 +0900

    Add ResponseObject

--- src/root.zig
+++ src/root.zig
@@ -1,24 +1,120 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
+const posix = std.posix;
+const net = std.net;
+const Thread = std.Thread;
+const Allocator = std.mem.Allocator;
+const testing = std.testing;
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.fs.File.stdout().deprecatedWriter();
-    // Buffering can improve performance significantly in print-heavy programs.
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+pub const JsonStream = @import("io.zig").JsonStream;
+pub const RpcClient = @import("rpc.zig").RpcClient;
+pub const RequestObject = @import("types.zig").RequestObject;
+pub const JsonRpcVersion = @import("types.zig").JsonRpcVersion;
+pub const RequestId = @import("types.zig").RequestId;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+test "call" {
+    const allocator = testing.allocator;
 
-    try bw.flush(); // Don't forget to flush!
+    const server_info = try spawn_server_thread(allocator);
+    defer {
+        allocator.destroy(server_info.context.address);
+        allocator.destroy(server_info.context);
+        allocator.destroy(server_info);
+    }
+
+    var client = try RpcClient.init(allocator, 4096, server_info.address);
+    defer client.deinit();
+
+    const request = try RequestObject.init(allocator, JsonRpcVersion.v2, "echo", null, RequestId{ .number = 1 });
+    defer request.deinit(allocator);
+    const response = try client.call(request);
+
+    try testing.expectEqualStrings("{\"jsonrpc\":\"2.0\",\"method\":\"echo\",\"id\":1}", response);
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
+const ServerInfo = struct {
+    thread: Thread,
+    address: std.net.Address,
+    context: *ServerContext,
+};
+
+const ServerContext = struct {
+    address: *std.net.Address,
+    allocator: std.mem.Allocator,
+};
+
+/// Spawn a server thread and returns the address
+/// Caller should destroy ServerInfo
+fn spawn_server_thread(allocator: Allocator) !*ServerInfo {
+    const address = try allocator.create(std.net.Address);
+    address.* = undefined;
+
+    const context = try allocator.create(ServerContext);
+    context.* = ServerContext{
+        .address = address,
+        .allocator = allocator,
+    };
+
+    // Spawn a thread
+    const thread = try std.Thread.spawn(.{}, serverThread, .{context});
+    // Wait a nano seconds until server activated
+    std.posix.nanosleep(1, 1000);
+
+    const server_info = try allocator.create(ServerInfo);
+    server_info.* = ServerInfo{
+        .thread = thread,
+        .address = address.*,
+        .context = context,
+    };
+    return server_info;
 }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+fn serverThread(context: *ServerContext) void {
+    runServer(context) catch |err| {
+        std.debug.print("Server error: {}\n", .{err});
+    };
+}
+
+/// This server will be shutdown after response at once
+fn runServer(context: *ServerContext) !void {
+
+    // Since we chose port: 0, OS will pick a port for us
+    const address = try std.net.Address.parseIp("127.0.0.1", 0);
+
+    const tpe: u32 = posix.SOCK.STREAM;
+    const protocol = posix.IPPROTO.TCP;
+    const listener = try posix.socket(address.any.family, tpe, protocol);
+    defer posix.close(listener);
+
+    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try posix.bind(listener, &address.any, address.getOsSockLen());
+
+    // Get a actual address that OS picked above
+    var actual_address: std.net.Address = undefined;
+    var len: posix.socklen_t = @sizeOf(net.Address);
+    try posix.getsockname(listener, &actual_address.any, &len);
+    context.address.* = actual_address;
+
+    try posix.listen(listener, 128);
+
+    while (true) {
+        var client_address: net.Address = undefined;
+        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+        const client_socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
+            std.debug.print("error accept: {}\n", .{err});
+            return err;
+        };
+        defer posix.close(client_socket);
+
+        var buf: [4096]u8 = undefined;
+        var jsonStream = JsonStream{
+            .buf = &buf,
+            .start = 0,
+            .pos = 0,
+        };
+        const request_message = try jsonStream.readMessage(client_socket);
+
+        try jsonStream.writeMessage(request_message, client_socket);
+    }
 }
--- src/types.zig
+++ src/types.zig
@@ -83,9 +83,9 @@ pub const RequestObject = struct {
         return req;
     }
 
-    pub fn deinit(s: *RequestObject, parent_alloc: Allocator) void {
-        s.arena.deinit();
-        parent_alloc.destroy(s);
+    pub fn deinit(self: *RequestObject, parent_alloc: Allocator) void {
+        self.arena.deinit();
+        parent_alloc.destroy(self);
     }
 
     /// Creates and allocates a new RequestObject with its own arena allocator.
@@ -168,6 +168,265 @@ pub const RequestObject = struct {
     }
 };
 
+/// A number that indicates the error type that occured.
+pub const ErrorCode = union(enum) {
+    parseError,
+    invalidRequest,
+    methodNotFound,
+    invalidParams,
+    internalError,
+    /// Reserved for implementation-defined server-errors.
+    serverError: i64,
+
+    /// Returns the value of the error code.
+    pub fn value(self: ErrorCode) i64 {
+        return switch (self) {
+            .parseError => -32700,
+            .invalidRequest => -32600,
+            .methodNotFound => -32601,
+            .invalidParams => -32602,
+            .internalError => -32603,
+            .serverError => |code| code,
+        };
+    }
+
+    /// Returns the ErrorCode from integer.
+    pub fn fromValue(code: i64) !ErrorCode {
+        return switch (code) {
+            -32700 => .parseError,
+            -32600 => .invalidRequest,
+            -32601 => .methodNotFound,
+            -32602 => .invalidParams,
+            -32603 => .internalError,
+            else => {
+                if (-32099 <= code and code <= -32000) {
+                    // Valid server error range.
+                    return .{ .serverError = @intCast(code) };
+                } else if (-32768 <= code and code < -32000) {
+                    // Reserved range for future use.
+                    return error.ReservedErrorCode;
+                } else {
+                    // Out of reserved range.
+                    return error.InvalidErrorCode;
+                }
+            },
+        };
+    }
+};
+
+pub const ErrorObject = struct {
+    /// Integer indicates the error thar occurred
+    code: ErrorCode,
+    /// Short description about the error
+    message: []const u8,
+    /// We can attach additional information about the errors
+    data: ?json.Value = null,
+
+    pub fn fromJsonObject(json_value: json.Value) !ErrorObject {
+        switch (json_value) {
+            .object => |obj| {
+                const code: ErrorCode = blk: {
+                    const code_value = obj.get("code") orelse return error.MissingErrorCode;
+                    switch (code_value) {
+                        .integer => |code| break :blk try ErrorCode.fromValue(code),
+                        else => return error.InvalidErrorCode,
+                    }
+                };
+
+                const message = blk: {
+                    const message_value = obj.get("message") orelse return error.MissingErrorMessage;
+                    switch (message_value) {
+                        .string => |msg| break :blk msg,
+                        else => return error.InvalidErrorMessage,
+                    }
+                };
+
+                const data: ?json.Value = obj.get("data");
+
+                return .{
+                    .code = code,
+                    .message = message,
+                    .data = data,
+                };
+            },
+            else => return error.InvalidErrorObject,
+        }
+    }
+};
+
+pub const ResponseObject = union(enum) {
+    ok: struct {
+        jsonrpc: JsonRpcVersion,
+        id: RequestId,
+        result: json.Value,
+        arena: std.heap.ArenaAllocator,
+    },
+    err: struct {
+        jsonrpc: JsonRpcVersion,
+        id: ?RequestId,
+        @"error": ErrorObject,
+        arena: std.heap.ArenaAllocator,
+    },
+
+    pub fn initOk(allocator: Allocator, jsonrpc: JsonRpcVersion, id: RequestId, result: json.Value) !*ResponseObject {
+        const response = try allocator.create(ResponseObject);
+        errdefer allocator.destroy(response);
+
+        response.* = ResponseObject{
+            .ok = .{
+                .jsonrpc = jsonrpc,
+                .id = id,
+                .result = result,
+                .arena = std.heap.ArenaAllocator.init(allocator),
+            },
+        };
+
+        return response;
+    }
+
+    pub fn initErr(allocator: Allocator, jsonrpc: JsonRpcVersion, id: ?RequestId, error_obj: ErrorObject) !*ResponseObject {
+        const response = try allocator.create(ResponseObject);
+        errdefer allocator.destroy(response);
+
+        response.* = ResponseObject{
+            .err = .{
+                .jsonrpc = jsonrpc,
+                .id = id,
+                .@"error" = error_obj,
+                .arena = std.heap.ArenaAllocator.init(allocator),
+            },
+        };
+
+        return response;
+    }
+
+    pub fn fromSlice(allocator: Allocator, json_string: []const u8) !*ResponseObject {
+        const response = try allocator.create(ResponseObject);
+        errdefer allocator.destroy(response);
+
+        var arena = std.heap.ArenaAllocator.init(allocator);
+        errdefer arena.deinit();
+
+        const parsed_json: json.Parsed(json.Value) = json.parseFromSlice(json.Value, arena.allocator(), json_string, .{}) catch |err| {
+            arena.deinit();
+            allocator.destroy(response);
+            return err;
+        };
+
+        const root = switch (parsed_json.value) {
+            .object => |obj| obj,
+            else => {
+                arena.deinit();
+                allocator.destroy(response);
+                return error.NotARequestObject;
+            },
+        };
+
+        const jsonrpc_value = root.get("jsonrpc") orelse return error.MissingProtocolVersion;
+        const jsonrpc = switch (jsonrpc_value) {
+            .string => |str| try JsonRpcVersion.fromString(str),
+            else => {
+                arena.deinit();
+                allocator.destroy(response);
+                return error.ProtocolVersionShouldBeString;
+            },
+        };
+
+        if (root.get("error")) |error_value| {
+            // This is an error response
+            const id: ?RequestId = blk: {
+                const id_value = root.get("id") orelse break :blk null;
+                switch (id_value) {
+                    .string => |str_id| break :blk RequestId{ .string = str_id },
+                    .integer => |int_id| break :blk RequestId{ .number = int_id },
+                    .null => break :blk null,
+                    else => {
+                        arena.deinit();
+                        allocator.destroy(response);
+                        return error.InvalidID;
+                    },
+                }
+            };
+
+            const error_object = try ErrorObject.fromJsonObject(error_value);
+
+            response.* = ResponseObject{ .err = .{
+                .jsonrpc = jsonrpc,
+                .id = id,
+                .@"error" = error_object,
+                .arena = arena,
+            } };
+        } else {
+            // This is an success response
+            const id: RequestId = blk: {
+                const id_value = root.get("id") orelse return error.MissingID;
+                switch (id_value) {
+                    .string => |str_id| break :blk RequestId{ .string = str_id },
+                    .integer => |int_id| break :blk RequestId{ .number = int_id },
+                    else => return error.InvalidID,
+                }
+            };
+            const result: json.Value = root.get("result") orelse return error.MissingResult;
+            response.* = ResponseObject{ .ok = .{
+                .jsonrpc = jsonrpc,
+                .id = id,
+                .result = result,
+                .arena = arena,
+            } };
+        }
+        return response;
+    }
+
+    pub fn toJson(self: *ResponseObject) ![]const u8 {
+        const allocator = blk: {
+            switch (self.*) {
+                .ok => break :blk self.ok.arena.allocator(),
+                .err => break :blk self.err.arena.allocator(),
+            }
+        };
+
+        var json_object = json.ObjectMap.init(allocator);
+        defer json_object.deinit();
+
+        switch (self.*) {
+            .ok => |ok| {
+                try json_object.put("jsonrpc", json.Value{ .string = ok.jsonrpc.toString() });
+                try json_object.put("id", ok.id.toJson());
+                try json_object.put("result", ok.result);
+            },
+            .err => |err| {
+                try json_object.put("jsonrpc", json.Value{ .string = err.jsonrpc.toString() });
+                if (err.id) |id| {
+                    try json_object.put("id", id.toJson());
+                }
+
+                // Create error object JSON
+                var error_obj = json.ObjectMap.init(allocator);
+
+                try error_obj.put("code", json.Value{ .integer = err.@"error".code.value() });
+                try error_obj.put("message", json.Value{ .string = err.@"error".message });
+                if (err.@"error".data) |data| {
+                    try error_obj.put("data", data);
+                }
+                try json_object.put("error", json.Value{ .object = error_obj });
+            },
+        }
+        const json_val = json.Value{ .object = json_object };
+
+        const str = try json.Stringify.valueAlloc(allocator, json_val, .{});
+
+        return str;
+    }
+
+    pub fn deinit(self: *ResponseObject, parent_alloc: Allocator) void {
+        switch (self.*) {
+            .ok => |ok| ok.arena.deinit(),
+            .err => |err| err.arena.deinit(),
+        }
+        parent_alloc.destroy(self);
+    }
+};
+
 test "RequestObject.fromSlice" {
     const allocator = testing.allocator;
 
@@ -216,6 +475,156 @@ test "RequestObject.toJson" {
     defer request.deinit(allocator);
 
     const actual = try request.toJson();
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "ResponseObject.fromSlice - success response" {
+    const allocator = testing.allocator;
+
+    const json_str =
+        \\{"jsonrpc":"2.0","id":1,"result":"success"}
+    ;
+
+    const response = try ResponseObject.fromSlice(allocator, json_str);
+    defer response.deinit(allocator);
+
+    try testing.expect(response.* == .ok);
+    const ok = response.ok;
+    try testing.expect(ok.jsonrpc == .v2);
+    try testing.expectEqualDeep(RequestId{ .number = 1 }, ok.id);
+
+    const result = ok.result;
+    try testing.expect(result == .string);
+    try testing.expectEqualStrings("success", result.string);
+}
+
+test "ResponseObject.fromSlice - success response with null result" {
+    const allocator = testing.allocator;
+
+    const json_str =
+        \\{"jsonrpc":"2.0","id":"test","result":null}
+    ;
+
+    const response = try ResponseObject.fromSlice(allocator, json_str);
+    defer response.deinit(allocator);
+
+    try testing.expect(response.* == .ok);
+    const ok = response.ok;
+    try testing.expect(ok.jsonrpc == .v2);
+    try testing.expectEqualDeep(RequestId{ .string = "test" }, ok.id);
+
+    const result = ok.result;
+    try testing.expect(result == .null);
+}
+
+test "ResponseObject.fromSlice - success response with object result" {
+    const allocator = testing.allocator;
+
+    const json_str =
+        \\{"jsonrpc":"2.0","id":42,"result":{"value":123,"message":"done"}}
+    ;
+
+    const response = try ResponseObject.fromSlice(allocator, json_str);
+    defer response.deinit(allocator);
+
+    try testing.expect(response.* == .ok);
+    const ok = response.ok;
+    try testing.expect(ok.jsonrpc == .v2);
+    try testing.expectEqualDeep(RequestId{ .number = 42 }, ok.id);
+
+    const result = ok.result;
+    try testing.expect(result == .object);
+
+    const result_obj = result.object;
+    const value = result_obj.get("value").?;
+    const message = result_obj.get("message").?;
+
+    try testing.expect(value == .integer);
+    try testing.expect(message == .string);
+    try testing.expectEqual(@as(i64, 123), value.integer);
+    try testing.expectEqualStrings("done", message.string);
+}
+
+test "ResponseObject.fromSlice - error response with string ID" {
+    const allocator = testing.allocator;
+
+    const json_str =
+        \\{"jsonrpc":"2.0","id":"req-1","error":{"code":-32601,"message":"Method not found"}}
+    ;
+
+    const response = try ResponseObject.fromSlice(allocator, json_str);
+    defer response.deinit(allocator);
+
+    try testing.expect(response.* == .err);
+    const err = response.err;
+    try testing.expect(err.jsonrpc == .v2);
+    try testing.expect(err.id != null);
+    try testing.expectEqualDeep(RequestId{ .string = "req-1" }, err.id.?);
+
+    const error_obj = err.@"error";
+    try testing.expect(error_obj.code == .methodNotFound);
+    try testing.expectEqualStrings("Method not found", error_obj.message);
+    try testing.expect(error_obj.data == null);
+}
+
+test "ResponseObject.fromSlice - error response with null ID" {
+    const allocator = testing.allocator;
+
+    const json_str =
+        \\{"jsonrpc":"2.0","id":null,"error":{"code":-32700,"message":"Parse error"}}
+    ;
+
+    const response = try ResponseObject.fromSlice(allocator, json_str);
+    defer response.deinit(allocator);
+
+    try testing.expect(response.* == .err);
+    const err = response.err;
+    try testing.expect(err.jsonrpc == .v2);
+    try testing.expect(err.id == null);
+
+    const error_obj = err.@"error";
+    try testing.expect(error_obj.code == .parseError);
+    try testing.expectEqualStrings("Parse error", error_obj.message);
+    try testing.expect(error_obj.data == null);
+}
+
+test "ResponseObject.toJson - success response" {
+    const allocator = testing.allocator;
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    const expected =
+        \\{"jsonrpc":"2.0","id":1,"result":"success"}
+    ;
+
+    var response = try ResponseObject.initOk(allocator, JsonRpcVersion.v2, RequestId{ .number = 1 }, json.Value{ .string = "success" });
+    defer response.deinit(allocator);
+
+    const actual = try response.toJson();
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "ResponseObject.toJson - error response" {
+    const allocator = testing.allocator;
+    const arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    const error_obj = ErrorObject{
+        .code = ErrorCode.methodNotFound,
+        .message = "Method not found",
+        .data = null,
+    };
+
+    var response = try ResponseObject.initErr(allocator, JsonRpcVersion.v2, RequestId{ .number = 1 }, error_obj);
+    defer response.deinit(allocator);
+
+    const expected =
+        \\{"jsonrpc":"2.0","id":1,"error":{"code":-32601,"message":"Method not found"}}
+    ;
+
+    const actual = try response.toJson();
 
     try testing.expectEqualStrings(expected, actual);
 }
